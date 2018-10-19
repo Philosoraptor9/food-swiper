0eb6eaa10d62a241c973926d2cd413a0a363fcdf
Philip Hoover <philhoov90@gmail.com>
2018-10-19 12:21:09 -0600

    test route working

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,29 +7,29 @@ router.get('/login', async (req, res) => {
     res.render('users/login.ejs');
 });
 
-router.post('/login', async (req, res)=> {
-    try {
-        const foundUser = await User.findOne(req.session.username);
-        if (foundUser){
-            if (await bcrypt.compare(req.body.password, foundUser.password)){
-                req.session.logged = true;
-                req.session.userId = foundUser._id;
-                res.redirect('/')
-            }
-            else {
-                req.session.message = 'incorrect username or password';
-                res.redirect('/auth/login')
-            }
-        }
-        else{
-            req.session.message = 'username or password is incorrect';
-            res.redirect('/');
-        }
-    }catch(err){
-        console.log(err);
-        res.send(err);
-    }
-});
+// router.post('/login', async (req, res)=> {
+//     try {
+//         const foundUser = await User.findOne(req.session.username);
+//         if (foundUser){
+//             if (await bcrypt.compare(req.body.password, foundUser.password)){
+//                 req.session.logged = true;
+//                 req.session.userId = foundUser._id;
+//                 res.redirect('./index')
+//             }
+//             else {
+//                 req.session.message = 'incorrect username or password';
+//                 res.redirect('/auth/login')
+//             }
+//         }
+//         else{
+//             req.session.message = 'username or password is incorrect';
+//             res.redirect('./index');
+//         }
+//     }catch(err){
+//         console.log(err);
+//         res.send(err);
+//     }
+// });
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const app = express();
-const expressSession = require('express-session');
 const mongoose = require('mongoose');
+const expressSession = require('express-session');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const bcrypt = require('bcryptjs');
+mongoose.set('useCreateIndex', true)
+
+
+const port = 3000;
 
 require('./db/db');
 
 app.use(methodOverride('_method'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(morgan);
+app.use(bodyParser.urlencoded({useNewUrlParser: true}));
+app.use(morgan('short'));
+
+
+const authController = require('./controllers/authController');
+app.use('/authController', authController);
+
+const foodController = require('./controllers/foodController');
+app.use('/foodController', foodController);
+
+const userController = require('./controllers/userController');
+app.use('/userController', userController);
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
   });
 
-app.listen(3000, () => {
-    console.log('App is listening on port 3000');
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
   });
