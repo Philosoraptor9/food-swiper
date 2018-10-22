450eb5592f988e8d435fe3087d1ac9606e5b2158
Philip Hoover <philhoov90@gmail.com>
2018-10-22 11:33:26 -0600

    user registration working

--- models/foods.js
+++ models/foods.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const foodSchema = new mongoose.Schema ({
-    name: {type: String, required: true},
-    rating: {type: Number},
+    foodName: {type: String, required: true},
+    restaurantName: {type: String, required: true},
+    image: {type: String, required: true},
     location: {type: String},
     category: {type: String},
+    rating: {type: Number},
     reviews: [{type: mongoose.Schema.Types.ObjectId, ref: 'Review'}]
 })
 
