a9dde44422094db804ec380bc9b99d08bfbfb0e1
connor mcdonough <connor.mcdonough28@gmail.com>
2018-10-24 11:26:24 -0600

    stuff

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const Foods= require('../models/foods');
-
+var Food= require('../models/foods');
 
 //Index Route
-router.get('/', (req, res) => {
-console.log(Foods);
-      res.render('index.ejs', { foods:Foods});
-
+router.get('/', async(req, res) => {
+      const food = await Food.find({});
+      res.render('index.ejs', { food:food});
 
 
 
 });
 
-//Detail/show route - i.e. specific food clicked
 
 
 
