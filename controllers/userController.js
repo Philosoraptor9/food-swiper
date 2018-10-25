fd85008c71eb601f68e042361311050925d61f09
Philip Hoover <philhoov90@gmail.com>
2018-10-24 22:00:05 -0600

    did some basic stylez

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,37 +23,48 @@ router.get('/new', (req, res) => {
     res.render('auth/login.ejs');
 });
 
+router.post('/', async (req, res)=>{
+    console.log(req.body.food);
+     try{
+     // console.log(req.body);
+     const hashedPassword = await bcrypt.hash(req.body.password, await bcrypt.genSalt(12));
+     // console.log(hashedPassword);
+     const newUser = {
+         username: req.body.username,
+         password: hashedPassword
+     }
+     const user = await User.create(newUser);
+     req.session.userId = user._id;
+     res.redirect('./index')
+     }catch(err){
+         res.send(err);
+     }
+ })
 
-// Edit User - put route??
-router.get('/:id/edit', requireLogin, async (req, res) => {
+// Show/edit profile route (will also show all the foods you've swiped right for)
+router.get('/:id', requireLogin, async (req, res) =>{
     try {
     const foundUser = await User.findById(req.session.userId);
-    res.render('users/edit.ejs', {
+    res.render('users/profile.ejs', {
         user: foundUser
         });
-    }catch(err){
+    }catch(err){a
         res.send(err)
     }
 });
 
-// Show profile route (will also show all the foods you've swiped right for)
-router.get('/:id', requireLogin, async (req, res) =>{
-    res.render('users/profile.ejs');
-});
+// Edit user
 
 // Delete User
 router.delete('/:id', requireLogin, async (req, res) => {
     try{
     const user = User.findByIdAndDelete(req.params.id);
-
         for (let i = 0; user.reviews.length; i++) {
             await Review.findByIdAndDelete(user.reviews[i]._id)
         }
-
         for (let i = 0; user.foods.length; i++) {
             await Food.findByIdAndDelete(user.foods[i]._id)
         }
-
         await User.findByIdAndDelete(req.params.id);
         res.redirect('/')
     } catch (err) {
@@ -61,22 +72,6 @@ router.delete('/:id', requireLogin, async (req, res) => {
     }
 })
 
-router.post('/', async (req, res)=>{
-   console.log(req.body.food);
-    try{
-    // console.log(req.body);
-    const hashedPassword = await bcrypt.hash(req.body.password, await bcrypt.genSalt(12));
-    // console.log(hashedPassword);
-    const newUser = {
-        username: req.body.username,
-        password: hashedPassword
-    }
-    const user = await User.create(newUser);
-    req.session.userId = user._id;
-    res.redirect('./index')
-    }catch(err){
-        res.send(err);
-    }
-})
+
 
 module.exports = router;
